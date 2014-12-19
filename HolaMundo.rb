457b5004e1c1c331532c9cda084b5085584df61b
rafaelrosalessanch <rafaelrosalessanchez@gmail.com>
2014-12-18 22:31:24 -0500

    Hola-Mundo
    Estoy subiendo el archivo hola mundo

---
+++ HolaMundo.rb
@@ -0,0 +1,17 @@
+class HolaMundo
+  def initialize()
+  end
+ # def salud()
+  #  puts "Hola Mundo"
+  #end
+ def salud()
+   nombre="1.8"
+   nombre=nombre.to_f
+   valorDos=2
+   valorDos=valorDos
+ puts nombre + valorDos
+ end
+end
+objeto=HolaMundo.new()
+objeto.salud
+gets()
